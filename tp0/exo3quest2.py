5d7f312b5b4ea8981695338113984cd86dcb1811
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4170-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 16:33:03 +0200

    ajout question 2 de l'exo3

--- tp0/exo3quest2.py
+++ tp0/exo3quest2.py
@@ -1,19 +1,17 @@
+"""question2 de l'exo 3"""
 releve1 = ("laser_avant", 2.35, "m")
 releve2 = ("laser_arriere", 1.10, "m")
 releve3 = ("gyroscope", 87.5, "deg")
 releves = [releve1, releve2, releve3]
-
-def recalibrer(releves,nom,val):
+def recalibrer(docreleve,nom,val):
+    """permet de changer la valeur d'un des composants"""
     newreleve=[]
-    for releve in releves:
+    for releve in docreleve:
         if releve[0]==nom:
-            newreleve.append((releve[0],val,releve[2]));
+            newreleve.append((releve[0],val,releve[2]))
         else:
-            newreleve.append(releve);
+            newreleve.append(releve)
     return newreleve
-
-
-
 nouveaux_releves = recalibrer(releves, "laser_avant", 2.40)
 assert nouveaux_releves[0] == ("laser_avant", 2.40, "m")
 assert nouveaux_releves[1] == releve2
