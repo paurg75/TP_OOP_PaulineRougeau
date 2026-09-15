96114521f79dcde6bec2724a6ce4454d31214511
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4170-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 15:49:01 +0200

    ajout exo 4

--- tp0/exo4.py
+++ tp0/exo4.py
@@ -19,3 +19,26 @@ exploration_seule = robots_exploration_seulement(robots_exploration, robots_tran
 assert double_mission == {"R5", "R7"}
 assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
+
+#question 2
+def ajouter_robot_mission(listrob,nom):
+    newlist=[]
+    newlist.append(listrob)
+    newlist.append(nom)
+    for i in newlist:
+        print(i)
+    return newlist
+
+def retirer_robot_mission(listrob,nom):
+    newlist=[]
+    for list in listrob:
+        if list!=nom:
+            newlist.append(list)        
+    return newlist
+    
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}
