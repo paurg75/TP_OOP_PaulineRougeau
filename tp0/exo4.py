b648ea7d4d281e0b069d26ff3f2832c793f9a53e
pltguest <pltguest@ad.polytech.sorbonne-universite.fr@pc4170-linux.ad.polytech.sorbonne-universite.fr>
2026-09-15 16:32:04 +0200

    ajout question 2 de l'exo4

--- tp0/exo4.py
+++ tp0/exo4.py
@@ -1,15 +1,18 @@
 #question1
-
+"""Exercice 4"""
 robots_exploration = {"R2", "R5", "R7"}
 robots_transport = {"R5", "R9", "R7", "R3"}
 def robots_double_mission(rob_exp,rob_trans):
+    """renvoie les robots qui font les 2 missions"""
     return rob_exp&rob_trans
 
 def robots_toutes_missions(rob_exp,rob_trans):
-    return rob_exp|rob_trans    
+    """renvoie tous les robots qui font au moins une mission"""
+    return rob_exp|rob_trans
 
 def robots_exploration_seulement(rob_exp,rob_trans):
-    return rob_exp-rob_trans 
+    """renvoie les robots qui ne font que la mission d'exploration"""
+    return rob_exp-rob_trans
 
 
 double_mission = robots_double_mission(robots_exploration, robots_transport)
@@ -21,24 +24,22 @@ assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
 
 #question 2
-def ajouter_robot_mission(listrob,nom):
-    newlist=[]
-    newlist.append(listrob)
-    newlist.append(nom)
-    for i in newlist:
-        print(i)
-    return newlist
+def ajouter_robot_mission(ensrob,nom):
+    """ajoute un robot à une mission"""
+    newens=ensrob.copy()
+    newens.add(nom)
+    return newens
 
-def retirer_robot_mission(listrob,nom):
-    newlist=[]
-    for list in listrob:
-        if list!=nom:
-            newlist.append(list)        
-    return newlist
-    
+def retirer_robot_mission(ensrob,nom):
+    """retire un robot à une mission"""
+    newens=ensrob.copy()
+    newens.remove(nom)
+    return newens
+
 ajout = ajouter_robot_mission(robots_exploration, "R8")
 retrait = retirer_robot_mission(robots_transport, "R9")
 assert ajout == {"R2", "R5", "R7", "R8"}
 assert retrait == {"R3", "R5", "R7"}
 # L’ensemble d’origine ne doit pas avoir été modifié
+print(robots_transport)
 assert robots_transport == {"R5", "R9", "R7", "R3"}
